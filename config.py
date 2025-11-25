2fa8dbd91caeea053a07e30d17afd549badd6fcc
slasjh <127065284+slasjh@users.noreply.github.com>
2025-11-25 11:56:47 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ source_urls = [
     "https://raw.githubusercontent.com/xmbjm/IPTV/refs/heads/master/output/user_result.txt",
         "http://156.238.251.122:7000",
     "https://156.238.251.122:888/live/dalao/iptv-Kimentanm.m3u",
-    "https://gitee.com/mytv-android/iptv-api/raw/master/output/result.m3u", 
+    "https://gitee.com/mytv-android/iptv-api/raw/master/output/result.m3u"
      #"http://156.238.251.122:888/live/live_Lite.txt",
     #"https://raw.githubusercontent.com/n3rddd/CTVLive/master/live.m3u",
   
