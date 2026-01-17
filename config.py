250c529a15fbb0bd5fafeffa604df3903a3c48c7
slasjh <127065284+slasjh@users.noreply.github.com>
2026-01-17 16:52:18 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ ip_version_priority = "ipv4"
 source_urls = [
    "https://raw.githubusercontent.com/Guovin/iptv-api/refs/heads/master/output/result.txt",
     "https://raw.githubusercontent.com/xmbjm/IPTV/refs/heads/master/output/user_result.txt",
-        "http://156.238.251.122:7000",
-      "http://64.81.114.28:7000",
+        "http://156.238.251.122:7000/txt",
+      "http://64.81.114.28:7000/txt",
     "https://156.238.251.122:888/live/dalao/iptv-Kimentanm.m3u",
     #"https://gitee.com/mytv-android/iptv-api/raw/master/output/result.m3u"
      #"http://156.238.251.122:888/live/live_Lite.txt",
