235a00db9e5f8489e230484d7c6d5f16cae6aef2
slasjh <127065284+slasjh@users.noreply.github.com>
2026-02-05 16:36:44 +0800

    Update config_three.py

--- config_three.py
+++ config_three.py
@@ -2,12 +2,11 @@ from datetime import datetime
 ip_version_priority = "ipv4"
 
 source_urls = [
-   "https://raw.githubusercontent.com/develop202/migu_video/refs/heads/main/interfaceTXT.txt",
    "https://raw.githubusercontent.com/slasjh/tvlive/refs/heads/main/checkspeed/whitelist_auto_tv.txt",
-    "http://156.238.251.122:888/live/fh.txt",
-    "https://github.com/slasjh/tvlive/raw/refs/heads/main/checkspeed/live_Lite.txt"
-    #"http://156.238.251.122:7000/txt",
-    #"http://64.81.114.28:7000/txt"
+    "http://156.238.251.122:7000/txt",
+    "http://64.81.114.28:7000/txt",
+    "https://raw.githubusercontent.com/xmbjm/IPTV/refs/heads/master/output/user_result.txt",
+    "https://raw.githubusercontent.com/Guovin/iptv-api/refs/heads/master/output/result.txt"
    
     #"https://gitee.com/mytv-android/iptv-api/raw/master/output/result.m3u"
      #"http://156.238.251.122:888/live/live_Lite.txt",
