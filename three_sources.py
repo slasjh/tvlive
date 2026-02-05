b4dd3ffa86a09cae727fe6a88cda5f3b558b69ed
slasjh <127065284+slasjh@users.noreply.github.com>
2026-02-05 16:29:47 +0800

    Update three_sources.py

--- three_sources.py
+++ three_sources.py
@@ -209,7 +209,7 @@ def updateChannelUrlsM3U(channels, template_channels):
                             # 保证数字连续
                             index = 1
                             for url in filtered_urls:
-                                url_suffix = f"$小土豆•IPV4" if len(filtered_urls) == 1 else f"$小土豆•『{index}』"
+                                url_suffix = f"$小土豆•IPV4" if len(filtered_urls) == 1 else f"$小土豆•IPV4『线路{index}』"
                                 if '$' in url:
                                     base_url = url.split('$', 1)[0]
                                 else:
